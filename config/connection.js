45645bbb852878a46b8ae84632b90e016ccbae30
dylecl <dylann.leclairr@gmail.com>
2019-04-12 21:59:49 -0600

    maybe now

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+
+    connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
     user: "root",
     password: "root",
     database: "burger_db2"
 });
 
+}
 
-
-connection.connect(function(err) {
-    if (err) {
-        console.log("error connecting:\n" + err);
-        return;
-    };
-    console.log("connected as ID: " + connection.threadId)
-})
+connection.connect();
 
 module.exports = connection;
